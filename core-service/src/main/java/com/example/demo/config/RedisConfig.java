7668a81181e54763a4878850a345c0efd96df0b2
saqlainansari <saqlainzarjisansari@gmail.com>
2026-04-26 18:04:11 +0530

    Add hardcoded Redis connection factory in Java config

--- core-service/src/main/java/com/example/demo/config/RedisConfig.java
+++ core-service/src/main/java/com/example/demo/config/RedisConfig.java
@@ -4,11 +4,19 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Hardcoded Redis connection for Render deployment
+        LettuceConnectionFactory factory = new LettuceConnectionFactory("red-d7mv0orbc2fs738grg0g", 6379);
+        return factory;
+    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
